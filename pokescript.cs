fb33353223a4311cbbfe0ddac86a2a4665f50224
David Medina <dav.medrin@gmail.com>
2019-11-19 22:10:51 -0500

    Cambio de clase unica a clases por grupo

--- pokescript.cs
+++ pokescript.cs
@@ -4,30 +4,49 @@ Made with a taste of love
 Based on https://bulbapedia.bulbagarden.net/wiki/Statistic
 */
 // Acaba este programa
-public class PokemonClass
+private class PetInfo {
+	public int number;
+	public string name;
+	public int level;
+	public int nature;
+
+	public PetStats baseStats;
+	public Stats stats;
+	public PetIndividualValues ivs;
+	public PetEffortValues evs;
+}
+
+private class Stats {
+	public float hitPoints;
+	public int attack;
+	public int defense;
+	public int specialAttack;
+	public int specialDefense; 
+	public int speed;
+}
+
+private class PetStats {
+	public Stats stats;
+}
+
+private class PetIndividualValues {
+	public Stats stats;
+}
+
+private class PetEffortValues {
+	public Stats stats;
+}
+
+public class PetClass
 {	
-	#Information
-	private int _number;
-	private string _name;
 	private int defaultAccuracy = 100;
 	private int defaultEvasion = 100;
 	
-	#Permanent stats
-	public float hitPoints { get; set;}
-	public int defense { get; set;}
-	public int attack { get; set;}
-	public int specialAttack { get; set;}
-	public int specialDefense { get; set;}
-	public int speed { get; set;}
 	#In-Battle Stats
 	public int evasion { get; set;}
 	public int accuracy { get; set;}
 	
 	#Base stats
-	public int level;
-	public int nature;
-	public Dictionary<int, int> individualValues;
-	public Dictionary<int, int> effortValues;
 	
 	private enum permanentStatName
 	{
@@ -38,58 +57,9 @@ public class PokemonClass
 		specialDefense,
 		speed
 	}
-	
-	public int number
-	{
-		get { return _number;}		
-		set { _number = value}		
-	}
-	
-	public int name
-	{
-		get { return _name;}		
-		set { _name = value}
-	}
-	
-	public void defineValues()
-	{
-		_number = -1
-		
-		hitPoints = 0.0f
-		attack = 0
-		defense = 0
-		specialAttack = 0
-		specialDefense = 0
-		speed = 1
-		
-		evasion = defaultEvasion
-		accuracy = defaultAccuracy
-		
-		individualValues = new Dictionary<int, int>(){
-			{permanentStatName.hitPoints , 1},
-			{permanentStatName.attack, 1},
-			{permanentStatName.defense, 1},
-			{permanentStatName.specialAttack, 1},
-			{permanentStatName.specialDefense, 1},
-			{permanentStatName.speed, 1}
-		}
-		
-		effortValues = new Dictionary<int, int>(){
-			{permanentStatName.hitPoints , 1},
-			{permanentStatName.attack, 1},
-			{permanentStatName.defense, 1},
-			{permanentStatName.specialAttack, 1},
-			{permanentStatName.specialDefense, 1},
-			{permanentStatName.speed, 1}
-		}
-	}
-	
-	public PokemonClass(){
-		defineValues()
-	}
 }
 
-class Pokemon: PokemonClass
+class Pet: PokemonClass
 {
 	public override string ToString()
 	{
